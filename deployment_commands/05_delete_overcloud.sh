1a8b8a33dd47c0c3e79ae16fe568ffe25819676e
hybridpollo <hybridpollo@proton.me>
2023-07-23 19:56:24 -0400

    fix idm integration template

--- deployment_commands/05_delete_overcloud.sh
+++ deployment_commands/05_delete_overcloud.sh
@@ -4,10 +4,12 @@
 # Variables
 ###################################################                                              
 STACK_NAME="voltron"
-THT_CUSTOM="/home/stack/rhosp_deployment/overcloud/deployment_templates"
+THT_CUSTOM="/home/stack/redhat_openstack_platform_17_templates/deployment_templates"
 BAREMETAL_NODE_FILE="${THT_CUSTOM}/baremetal_node_deployment/baremetal_deployment.yaml"
 NETWORK_DATA_FILE="${THT_CUSTOM}/baremetal_node_deployment/network_data.yaml"
 
+
+# load the stackrc file environment variables for the undercloud
 source /home/stack/stackrc
 
 ###################################################                                                               
